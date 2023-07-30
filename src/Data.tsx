e8756fa7f6cf4fbbca949a87943dc0cca45b41a4
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-07-30 22:39:43 +0530

    About Page

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,4 +1,4 @@
-import {  FaDribbble, FaInstagram, FaLinkedin } from "react-icons/fa";
+import {  FaUser, FaPhoneAlt, FaPaperPlane, FaDribbble, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { BsMedium } from "react-icons/bs";
 
 export const navLinks = ["home", "about", "skills", "portfolio", "contact"]
@@ -9,3 +9,24 @@ export const socialIcons = [
     <FaLinkedin />,
     <BsMedium />
 ]
+
+export const bios = [
+    {
+      id: 1,
+      icon: <FaUser />,
+      key: "Name",
+      value: "Saket"
+    },
+    {
+      id: 2,
+      icon: <FaPhoneAlt />,
+      key: "Phone",
+      value: "+919950262900"
+    },
+    {
+      id: 3,
+      icon: <FaPaperPlane />,
+      key: "Email",
+      value: "saketaryan2002@gmail.com"
+    }
+  ]
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import portfolio from "../../assets/profile.jpeg"
 import "./Home.scss"
 import { motion } from 'framer-motion'
