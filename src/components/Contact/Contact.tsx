9dd9031eb0bd7173e0925eb6b48de216f69289a4
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-07-31 01:27:03 +0530

    Contact Form Updated

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,80 +1,124 @@
+import { useRef } from "react";
 import "./Contact.scss";
-import { contacts } from '../../Data';
-import { socialIcons } from '../../Data';
-import { motion } from 'framer-motion';
+import { contacts } from "../../Data";
+import { socialIcons } from "../../Data";
+import { motion } from "framer-motion";
 
 const Contact = () => {
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const phoneRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const firstName = firstNameRef.current!.value;
+    const lastName = lastNameRef.current!.value;
+    const phone = phoneRef.current!.value;
+    const email = emailRef.current!.value;
+    const message = messageRef.current!.value;
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      phone: phone,
+      email: email,
+      message: message,
+    };
+    fetch(import.meta.env.VITE_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          alert("Message Sent. Thank you for contacting me.");
+          firstNameRef.current!.value = "";
+          lastNameRef.current!.value = "";
+          phoneRef.current!.value = "";
+          emailRef.current!.value = "";
+          messageRef.current!.value = "";
+        } else {
+          console.log(data);
+          alert("Message failed to send. Please try again.");
+        }
+      });
+  };
+
   return (
     <div className="container" id="contact">
       <motion.div
-        initial={{opacity: 0}}
-        whileInView={{y: [-50, 0], opacity: 1}} 
+        initial={{ opacity: 0 }}
+        whileInView={{ y: [-50, 0], opacity: 1 }}
         className="title"
       >
-            <span>get in touch</span>
-            <h1>Contact Me</h1>
+        <span>get in touch</span>
+        <h1>Contact Me</h1>
       </motion.div>
       <div className="contact_form">
         <motion.div
-          initial={{x: 0, opacity: 0}}
-          whileInView={{ x: [-150,0], opacity: 1 }}
-          transition={{duration: 1}}
-          className='contact_left_container'>
+          initial={{ x: 0, opacity: 0 }}
+          whileInView={{ x: [-150, 0], opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="contact_left_container"
+        >
           <h3>Just Say Hi</h3>
-          <p className='contact_text'>
-            I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with React, React Native and Node.js.
-            I also have experience working with Python, Java, Nextjs.
+          <p className="contact_text">
+            I am available on almost every social media. You can message me, I
+            will reply within 24 hours. I can help you with React, React Native
+            and Node.js. I also have experience working with Python, Java,
+            Nextjs.
           </p>
-          {contacts.map(contact => {
+          {contacts.map((contact) => {
             return (
-              <div className='contact_left' key={contact.id}>
-                <div className="icon">
-                  {contact.icon}
-                </div>
-                <p>{ contact.infoText}</p>
+              <div className="contact_left" key={contact.id}>
+                <div className="icon">{contact.icon}</div>
+                <p>{contact.infoText}</p>
               </div>
-            )
+            );
           })}
           <div className="social_icons">
             {socialIcons.map((socialIcon, index) => {
-            return (
-              <div key={index} >
-                <a href={socialIcon.url}>{socialIcon.icon}</a>
-              </div>
-            )
-          })}
+              return (
+                <div key={index}>
+                  <a href={socialIcon.url}>{socialIcon.icon}</a>
+                </div>
+              );
+            })}
           </div>
         </motion.div>
         <motion.div
-          initial={{x: 0, opacity: 0}}
-          whileInView={{ x: [150,0], opacity: 1 }}
-          transition={{duration: 1}}
+          initial={{ x: 0, opacity: 0 }}
+          whileInView={{ x: [150, 0], opacity: 1 }}
+          transition={{ duration: 1 }}
           className="contact_right"
-        
         >
           <h3>Get In Touch</h3>
           <div className="row">
-            <input type="text" placeholder='First Name' />
-            <input type="text" placeholder='Last name'/>
+            <input type="text" placeholder="First Name" ref={firstNameRef} />
+            <input type="text" placeholder="Last name" ref={lastNameRef} />
           </div>
           <div className="row">
-            <input type="text" placeholder='Phone' />
-            <input type="email" placeholder='Email' />
+            <input type="text" placeholder="Phone" ref={phoneRef} />
+            <input type="email" placeholder="Email" ref={emailRef} />
           </div>
           <div className="row">
-            <textarea placeholder='message'></textarea>
+            <textarea placeholder="message" ref={messageRef}></textarea>
           </div>
           <motion.div
             whileHover={{ scale: 1.1 }}
-            transition={{duration: 0.3}}
+            transition={{ duration: 0.3 }}
             className="btn"
           >
-            <a href="#">Send</a>
+            <a onClick={handleSubmit}>Send</a>
           </motion.div>
         </motion.div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
