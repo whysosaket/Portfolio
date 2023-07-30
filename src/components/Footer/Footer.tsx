d04b4575dc05c485451612d3e89e9f7b910d61b2
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-07-30 23:23:21 +0530

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import "./Footer.scss"
+import { socialIcons } from '../../Data';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{
+        opacity:
+          1
+      }}
+      transition={{duration: 1.5}}
+      className='footer'
+    >
+      <div className="copyright">
+        <p>I am<span> Saket Aryan</span></p>
+      </div>
+      <div className="followMe">
+        <h4>Follow Me</h4>
+        <div className="stick"></div>
+        <div className="social_icons">
+            {socialIcons.map((socialIcon, index) => {
+            return (
+              <div key={index} >
+                <a href={socialIcon.url}> {socialIcon.icon} </a>
+              </div>
+            )
+          })}
+          </div>
+      </div>
+    </motion.div>
   )
 }
 
